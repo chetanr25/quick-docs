33e574673d455e3dccc789e5613201df19f66776
chetanr25 <--replace-all>
2025-09-09 14:55:38 +0530

    Upgraded version

--- quick_doc_app/android/app/build.gradle.kts
+++ quick_doc_app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.quick_docs"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.0.12433566"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
